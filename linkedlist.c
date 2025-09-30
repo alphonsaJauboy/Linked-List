fee6c70f341444d9c0cbeeb0cb7db6d94c95ad96
alphonsaJauboy <alphonsajauboy2029@mca.ajce.in>
2025-09-30 14:39:36 +0530

    linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -10,10 +10,23 @@ struct list* createNode(int a){
     node->next=NULL;
     return node;
 }
+void print(struct list*head){
+    struct list*temp=head;
+    while(temp!=NULL){
+        printf("%d -> ",temp->n);
+        temp=temp->next;
+    }
+    printf("NULL\n");
+}
 int main(){
     struct list* head=createNode(10);
     struct list* second=createNode(20);
     head->next=second;
     second->next=NULL;
+    print(head);
     return 0;
 }
+
+
+  
+   
